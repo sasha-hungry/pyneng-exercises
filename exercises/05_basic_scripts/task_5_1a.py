5eb806bf9f2304452148580c15ce5f2c5289c1dc
sasha-hungry <argus2005@inbox.ru>
2021-03-16 17:15:39 +0300

    done task_5-1a

--- exercises/05_basic_scripts/task_5_1a.py
+++ exercises/05_basic_scripts/task_5_1a.py
@@ -44,3 +44,9 @@ london_co = {
         "routing": True,
     },
 }
+
+device = input ("Введите имя устрйоства (r1, r2, sw1):")
+param = input ("Введите имя параметра: ")
+
+print(london_co[device][param])
+
