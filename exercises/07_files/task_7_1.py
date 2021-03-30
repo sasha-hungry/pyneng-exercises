ff37a4a0a0749bdbcd930e163d7f16b4d0b90efe
sasha-hungry <argus2005@inbox.ru>
2021-03-30 14:22:55 +0300

    done task_7-1

--- exercises/07_files/task_7_1.py
+++ exercises/07_files/task_7_1.py
@@ -12,5 +12,23 @@ Last update           3d18h
 Outbound Interface    FastEthernet0/0
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
-
+O        10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0
 """
+
+
+output = """
+            Prefix                {}
+            AD/Metric             {}
+            Next-Hop              {}
+            Last update           {}
+            Outbound Interface    {} """
+        
+
+
+with open('ospf.txt') as f:
+    for line in f:
+        line = line.replace('[',' ').replace(']',' ').replace(',','').split()
+        print(output.format(line[1],line[2],line[4],line[5],line[6]))
+            
+             
+    
