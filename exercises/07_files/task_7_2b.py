00d79e541d524babc2321eadc4eef34e840efe42
sasha-hungry <argus2005@inbox.ru>
2021-03-30 17:46:46 +0300

    выполнено 7-2b

--- exercises/07_files/task_7_2b.py
+++ exercises/07_files/task_7_2b.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Задание 7.2b
@@ -17,3 +18,15 @@
 """
 
 ignore = ["duplex", "alias", "configuration"]
+
+from sys import argv
+
+input_filename = argv[1]
+output_filename = argv[2]
+
+with open(input_filename) as src, open(output_filename, 'w') as dest:
+    for line in src:
+        command = line.split()
+        ingnore_include = set(command)&set(ignore) # списоки конвертируются во множеста и находится их пересечение
+        if not line.startswith('!') and not ingnore_include:  # если строка не начиниается с ! и пересечние списка ignored со словами из строки то print (наличе какого либо значения для python есть true)
+            dest.write(line)
