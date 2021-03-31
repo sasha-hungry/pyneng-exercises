e49913493ca6b8074f44cf792b6a34af2260a302
sasha-hungry <argus2005@inbox.ru>
2021-03-31 16:31:00 +0300

    выполнено задание 7-3b

--- exercises/07_files/task_7_3b.py
+++ exercises/07_files/task_7_3b.py
@@ -17,3 +17,15 @@ Enter VLAN number: 10
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+result=[]
+input_vlan = input('Enter VLAN number:')
+
+with open('CAM_table.txt') as f:
+    for line in f:
+        line_list=line.split()
+        if line_list and line_list[0].isdigit() :
+            result.append([int(line_list[0]),line_list[1],line_list[3]]) #добавляем прямо список, номер vlan записываем как число
+
+for vlan,mac,interface in result:
+    if int(input_vlan) == vlan :
+        print('{:<9} {:20} {}'.format(vlan,mac,interface))
